5c2e47a1d190fcd2d1495090996d4f0e45525a7b
zhanglinguan <864290799@qq.com>
2022-03-07 10:19:07 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhanglinguang/go_1
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,8 @@
+package go_1
+
+import "fmt" 
+
+// say Hi to someone
+func SayHi(name string) string {
+   return fmt.Sprintf("Hi, %s", name)
+}
